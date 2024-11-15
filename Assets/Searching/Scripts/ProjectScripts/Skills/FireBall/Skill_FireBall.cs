7793742aac67965f7eb7b342b2a4325e09cd06ba
PoomBu1650702234 <phuphan.sang@bumail.net>
2024-11-15 18:21:58 +0700

    make skill obj

--- Assets/Searching/Scripts/ProjectScripts/Skills/FireBall/Skill_FireBall.cs
+++ Assets/Searching/Scripts/ProjectScripts/Skills/FireBall/Skill_FireBall.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Skill_FireBall : MonoBehaviour
 {
+    private Rigidbody2D rd; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
