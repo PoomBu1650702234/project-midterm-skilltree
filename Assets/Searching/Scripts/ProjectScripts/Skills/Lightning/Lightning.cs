508d8f3d89d3911958ab282bb32883969cc84743
PoomBu1650702234 <phuphan.sang@bumail.net>
2024-11-18 16:14:30 +0700

    set up thunder skill tree backend

---
+++ Assets/Searching/Scripts/ProjectScripts/Skills/Lightning/Lightning.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : Skill
+{
+    public override void Activate()
+    {
+        base.Activate();
+    }
+}
