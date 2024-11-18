b8300a2c6825da0a31b5f47650316580889f5a4f
PoomBu1650702234 <phuphan.sang@bumail.net>
2024-11-18 20:35:21 +0700

    Done all Lightning tree

--- Assets/Searching/Scripts/ProjectScripts/Skills/Lightning/ThunderShoot.cs
+++ Assets/Searching/Scripts/ProjectScripts/Skills/Lightning/ThunderShoot.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class ThunderShoot : Skill
 {
+    [SerializeField] GameObject skillPref;
     public override void Activate()
     {
-        base.Activate();
+        if(skillPref == null)
+        {
+            return;
+        }
+
+        for(int i = 0; i < 4 ; i++)
+        {
+            StartCoroutine(DamageCouroutine(i));
+        }
+    }
+
+    private IEnumerator DamageCouroutine(float second)
+    {
+        yield return new WaitForSeconds(second);
+        SpawnLightning();
+        
+    }
+
+    private void SpawnLightning()
+    {
+        GameObject skill = Instantiate(skillPref);
+        skill.transform.position = MockUpPlayer.instance.transform.position + new Vector3(Random.Range(5, -5), Random.Range(5, -5),0);
+        skill.transform.rotation = Quaternion.Euler(
+            0, 
+            0, 
+            Random.Range(-360f, 360f)
+        );
+
+        ThunderShoot_Skill thunderShoot_Skill = skill.GetComponent<ThunderShoot_Skill>();
+        thunderShoot_Skill.spawnedSkillClass = this;
     }
 }
